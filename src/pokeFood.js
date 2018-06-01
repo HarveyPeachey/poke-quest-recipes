886eb65bdbb95bba79020cd4ba6ae1f08fd3bad5
Theruler333 <harveypeachey@hotmail.co.uk>
2018-06-01 16:45:25 +0100

    Added pokeFood array directory

---
+++ src/pokeFood.js
@@ -0,0 +1,62 @@
+export const pokeFood = [
+  {
+    id: "6",
+    name: "Mouth-Watering Dip a la Cube",
+    type: "Water",
+  },
+  {
+    id: "7",
+    name: "Plain Crepe a la Cube",
+    type: "Normal",
+  },
+  {
+    id: "8",
+    name: "Sludge Soup a la Cube",
+    type: "Poison",
+  },
+  {
+    id: "9",
+    name: "Mud Pie a la Cube",
+    type: "Ground",
+  },
+  {
+    id: "10",
+    name: "Veggie Smoothie a la Cube",
+    type: "Grass",
+  },
+  {
+    id: "11",
+    name: "Honey Nectar a la Cube",
+    type: "Bug",
+  },
+  {
+    id: "12",
+    name: "Brain Food a la Cube",
+    type: "Psychic",
+  },
+  {
+    id: "13",
+    name: "Stone Soup a la Cube",
+    type: "Rock",
+  },
+  {
+    id: "14",
+    name: "Light-as-Air Casserole a la Cube",
+    type: "Flying",
+  },
+  {
+    id: "15",
+    name: "Hot Pot a la Cube",
+    type: "Fire",
+  },
+  {
+    id: "16",
+    name: "Watt a Risotta a la Cube",
+    type: "Electric",
+  },
+  {
+    id: "17",
+    name: "Get Swole Syrup a la Cube",
+    type: "Fighting",
+  }
+];
