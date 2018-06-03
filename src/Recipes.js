e4d0cee9ffcf7de21ad06613ff4cea3b378ce8cc
Theruler333 <harveypeachey@hotmail.co.uk>
2018-06-03 18:35:43 +0100

    Added remaining recipes from SkaCubby’s graphic

--- src/Recipes.js
+++ src/Recipes.js
@@ -4,20 +4,28 @@ export const Recipes = [
      "recipe_name":"Mouth-Watering Dip a la Cube",
      "set_ingredients":[
        {
+         "components":["ir","bb","bb","h","h"],
+         "quality": "Very Good"
+       },
+       {
          "components":["bb","bb","bb","tm","tm"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["bb","bb","bb","br","br"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["bb","bb","ir","tm","tm"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["bb","bb","bb","h","bm"],
-         "quality": ""
+         "quality": "Basic"
+       },
+       {
+         "components":["bb","bb","h","h","a"],
+         "quality": "Basic"
        }
      ]
   },
@@ -26,8 +34,20 @@ export const Recipes = [
      "recipe_name":"Plain Crepe a la Cube",
      "set_ingredients":[
        {
+         "components":["h","h","h","bm","bm"],
+         "quality": "Special"
+       },
+       {
+         "components":["h","h","bb","bm","bm"],
+         "quality": "Very Good"
+       },
+       {
+         "components":["bb","bb","h","f","f"],
+         "quality": "Good"
+       },
+       {
          "components":["bb","bb","bb","f","f"],
-         "quality": ""
+         "quality": "Basic"
        }
      ]
   },
@@ -37,7 +57,11 @@ export const Recipes = [
      "set_ingredients":[
        {
          "components":["tm","tm","tm","bm","bm"],
-         "quality": ""
+         "quality": "Basic"
+       },
+       {
+         "components":["bm","bm","bm","tm","tm"],
+         "quality": "Basic"
        }
      ]
   },
@@ -46,28 +70,48 @@ export const Recipes = [
      "recipe_name":"Mud Pie a la Cube",
      "set_ingredients":[
        {
+         "components":["h","h","h","ir","ir"],
+         "quality": "Special"
+       },
+       {
+         "components":["br","br","br","ir","ir"],
+         "quality": "Special"
+       },
+       {
+         "components":["br","bb","bm","f","ir"],
+         "quality": "Very Good"
+       },
+       {
+         "components":["ir","ir","h","h","bb"],
+         "quality": "Very Good"
+       },
+       {
+         "components":["tm","tm","tm","ir","ir"],
+         "quality": "Good"
+       },
+       {
          "components":["tm","tm","tm","f","f"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["bb","bb","f","f","tm"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["ir","ir","h","tm","bb"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["tm","tm","ir","h","f"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["bb","bb","tm","tm","h"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["f","f","bb","tm","br"],
-         "quality": ""
+         "quality": "Basic"
        }
      ]
   },
@@ -76,16 +120,20 @@ export const Recipes = [
      "recipe_name":"Veggie Smoothie a la Cube",
      "set_ingredients":[
        {
+         "components":["br","br","br","a","a"],
+         "quality": "Very Good"
+       },
+       {
          "components":["br","br","br","bm","a"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["br","bm","a","a","a"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["br","br","a","a","f"],
-         "quality": ""
+         "quality": "Basic"
        }
      ]
   },
@@ -94,12 +142,16 @@ export const Recipes = [
      "recipe_name":"Honey Nectar a la Cube",
      "set_ingredients":[
        {
-         "components":["h","h","h","bb","bb"],
-         "quality": ""
+         "components":["h","h","h","ir","bb"],
+         "quality": "Very Good"
        },
        {
          "components":["bb","bb","h","h","a"],
-         "quality": ""
+         "quality": "Good"
+       },
+       {
+         "components":["h","h","h","bb","bb"],
+         "quality": "Basic"
        }
      ]
   },
@@ -130,20 +182,32 @@ export const Recipes = [
      "recipe_name":"Stone Soup a la Cube",
      "set_ingredients":[
        {
+         "components":["ir","ir","ir","f","a"],
+         "quality": "Very Good"
+       },
+       {
+         "components":["ir","ir","f","a","a"],
+         "quality": "Good"
+       },
+       {
          "components":["f","f","f","a","a"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["f","f","ir","ir","br"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["f","f","f","a","ir"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["f","f","a","a","bb"],
-         "quality": ""
+         "quality": "Basic"
+       },
+       {
+         "components":["f","f","f","bb","a"],
+         "quality": "Basic"
        }
      ]
   },
@@ -152,8 +216,16 @@ export const Recipes = [
      "recipe_name":"Light-as-Air Casserole a la Cube",
      "set_ingredients":[
        {
+         "components":["ir","ir","ir","br","br"],
+         "quality": "Special"
+       },
+       {
          "components":["f","f","br","br","ir"],
-         "quality": ""
+         "quality": "Basic"
+       },
+       {
+         "components":["f","f","f","br","br"],
+         "quality": "Basic"
        }
      ]
   },
@@ -162,12 +234,24 @@ export const Recipes = [
      "recipe_name":"Hot Pot a la Cube",
      "set_ingredients":[
        {
+         "components":["tm","tm","bm","bb",bb],
+         "quality": "Good"
+       },
+       {
+         "components":["tm","tm","tm","h","h"],
+         "quality": "Good"
+       },
+       {
+         "components":["tm","tm","tm","bm","bm"],
+         "quality": "Basic"
+       },
+       {
          "components":["tm","tm","tm","bb","bb"],
-         "quality": ""
+         "quality": "Basic"
        },
        {
          "components":["tm","tm","tm","a","a"],
-         "quality": ""
+         "quality": "Basic"
        }
      ]
   },
@@ -176,8 +260,12 @@ export const Recipes = [
      "recipe_name":"Watt a Risotta a la Cube",
      "set_ingredients":[
        {
+         "components":["h","h","br","bm","a"],
+         "quality": "Very Good"
+       },
+       {
          "components":["tm","tm","h","h","a"],
-         "quality": ""
+         "quality": "Basic"
        }
      ]
   },
@@ -186,8 +274,12 @@ export const Recipes = [
      "recipe_name":"Get Swole Syrup a la Cube",
      "set_ingredients":[
        {
+         "components":["bb","bb","h","tm","bm"],
+         "quality": "Good"
+       },
+       {
          "components":["bb","bb","h","tm","tm"],
-         "quality": ""
+         "quality": "Basic"
        }
      ]
   }
