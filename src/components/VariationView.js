3d456a0185f0da098316763499e3313e21f4b7b2
Theruler333 <harveypeachey@hotmail.co.uk>
2018-06-27 11:33:10 +0100

    Removed console log

--- src/components/VariationView.js
+++ src/components/VariationView.js
@@ -26,7 +26,6 @@ const VariationView = ({variations, pokeSprites, handlePokeClick}) => {
       }
     }
   }
-  console.log(variationPokemon);
   const variationPokemonImage = variationPokemon.map((variationPokemonMap, index) => {
     return (
       <PokemonItem
@@ -36,7 +35,6 @@ const VariationView = ({variations, pokeSprites, handlePokeClick}) => {
       />
     );
   })
-  console.log(variationPokemonImage);
   switch(quality) {
     case "Special":
       basicBronze = 6
