7865c8f72131ca8b4c3901020e380ee570fb1f6e
Harvey Peachey <harvey.peachey@bbc.co.uk>
2020-07-10 14:51:19 +0100

    Implemented styled components for main App

--- src/app/containers/App/App.js
+++ src/app/containers/App/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+const App = () => {
+  const StyledDiv = styled.div`
+    text-align: center;
+  `;
+
+  const StyledHeader = styled.div`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <StyledDiv>
+      <StyledHeader/>
+    </StyledDiv>
   );
 }
 
